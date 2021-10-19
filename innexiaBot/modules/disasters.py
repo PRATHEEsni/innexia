57dfc16ebb44f0717b81d9359d08de6503b7e342
anonymous <89799912+PRATHEEsni@users.noreply.github.com>
2021-10-19 20:09:46 +0530

    Update disasters.py

--- innexiaBot/modules/disasters.py
+++ innexiaBot/modules/disasters.py
@@ -539,7 +539,7 @@ def devlist(update: Update, context: CallbackContext):
         "<code>Gathering intel..</code>", parse_mode=ParseMode.HTML
     )
     true_dev = list(set(DEV_USERS) - {OWNER_ID})
-    reply = "<b>Hero Association Members ⚡️:</b>\n"
+    reply = "<b>Pratheek Association Members 🥀:</b>\n"
     for each_user in true_dev:
         user_id = int(each_user)
         try:
